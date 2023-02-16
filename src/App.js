fde725717ff24b1736acf80a85500f7a4c39c9c9
merickagustin <merickic.agustin@gmail.com>
2023-02-16 17:08:41 +0800

    Add configurations for customize bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./css/custom.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "./css/style.css";
+
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header sticky-top">
+        <Navbar />
       </header>
+      <section></section>
     </div>
   );
 }
